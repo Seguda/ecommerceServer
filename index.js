f59f05f21c0225a8252d68e5f830fcf814caae02
Seha Dalay <sehagulnar@gmail.com>
2023-03-27 13:48:17 -0400

    Created Roots and models

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
+const userRoute = require("./routes/user");
+const authRoute = require("./routes/auth");
 
 dotenv.config();
 
@@ -13,6 +15,10 @@ mongoose
     console.log(err);
   });
 
-app.listen(process.env.POT || 5000, () => {
+app.use(express.json());
+app.use("/api/users", userRoute);
+app.use("/api/auth", authRoute);
+
+app.listen(process.env.PORT || 5000, () => {
   console.log("Backend Server is running!");
 });
