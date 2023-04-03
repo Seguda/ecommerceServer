cba2e85c53f5fe9f3dfe87b019d19f24e2b6fc74
Seha Dalay <sehagulnar@gmail.com>
2023-04-03 15:39:57 -0400

    Created Update method

--- routes/user.js
+++ routes/user.js
@@ -1,3 +1,29 @@
+const User = require("../models/User");
+const { verifyToken, verifyTokenAndAuthorization } = require("./verifyToken");
+
 const router = require("express").Router();
+
+//Update
+router.put("/:id", verifyTokenAndAuthorization, async (req, res) => {
+  if (req.body.password) {
+    req.body.password = CryptoJS.AES.encrypt(
+      req.body.password,
+      process.env.Password_Sec
+    ).toString();
+  }
+  try {
+    const updatedUser = await User.findByIdAndUpdate(
+      req.params.id,
+      {
+        $set: req.body,
+      },
+      { new: true }
+    );
+    res.status(200).json(updatedUser);
+  } catch (err) {
+    console.log(res);
+    res.status(500).json(err);
+  }
+});
 
 module.exports = router;
